145878e7ce67215c4b5c9c034f45977afe20bb4a
orItach <or_ith@walla.com>
2018-09-22 23:52:26 +0300

    Add project files.

---
+++ BooksStore/BooksStore/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BooksStore.Models;
+
+namespace BooksStore.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
