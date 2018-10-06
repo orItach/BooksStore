34bedf288c1cea7958e6832470a13549b865ca16
orItach <or_ith@walla.com>
2018-10-07 00:35:54 +0300

    view model for new class (books and category)

---
+++ BooksStore/BooksStore/ViewModels/BooksViewModel.cs
@@ -0,0 +1,58 @@
+﻿using BooksStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BooksStore.ViewModels
+{
+    public class BooksViewModel : BaseViewModel
+    {
+        public ObservableCollection<Book> Books { get; set; }
+        public Command LoadBooksCommand { get; set; }
+
+        public BooksViewModel()
+        {
+            Title = "Libery";
+            Books = new ObservableCollection<Book>();
+            LoadBooksCommand = new Command(async () => await ExecuteLoadBooksCommand());
+
+            //MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
+            //{
+            //    var newItem = item as Item;
+            //    Items.Add(newItem);
+            //    await DataStore.AddItemAsync(newItem);
+            //});
+        }
+
+        async Task ExecuteLoadBooksCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Books.Clear();
+                var items = await DataStore.GetBooksAsync(true);
+                foreach (var item in items)
+                {
+                    Books.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+   
+}
---
+++ BooksStore/BooksStore/ViewModels/MainCategoriesViewModel.cs
@@ -0,0 +1,57 @@
+﻿using BooksStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BooksStore.ViewModels
+{
+    public class MainCategoriesViewModel : BaseViewModel
+    {
+        public ObservableCollection<MainCategory> MainCategories { get; set; }
+        public Command LoadBooksCommand { get; set; }
+
+        public MainCategoriesViewModel()
+        {
+            Title = "Preferences";
+            MainCategories = new ObservableCollection<MainCategory>();
+            LoadBooksCommand = new Command(async () => await ExecuteLoadMainCategoriesCommand());
+
+            //MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
+            //{
+            //    var newItem = item as Item;
+            //    Items.Add(newItem);
+            //    await DataStore.AddItemAsync(newItem);
+            //});
+        }
+
+        async Task ExecuteLoadMainCategoriesCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                MainCategories.Clear();
+                var items = await DataStore.GetMainCategoriesAsync(true);
+                foreach (var item in items)
+                {
+                    MainCategories.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
